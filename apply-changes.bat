10a4a1968eb82497561b365a2daeb0509bf59e82
rmtteam <48514632+rmtteam@users.noreply.github.com>
2023-04-21 13:40:06 +0300

    Еще что то

--- apply-changes.bat
+++ apply-changes.bat
@@ -1,5 +1,6 @@
 @echo off
 xcopy /y desktopinfo.ini "%PROGRAMFILES%\Desktopinfo"
 xcopy /y scripts\* "%PROGRAMFILES%\Desktopinfo\scripts"
+xcopy /y qr\* "%PROGRAMFILES%\Desktopinfo\qr"
 echo Configuration file updated!
 pause
