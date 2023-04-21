9042a93ef9880caecc44ca5bd730859a4f786a21
rmtteam <48514632+rmtteam@users.noreply.github.com>
2023-04-21 13:07:23 +0300

    Teamviewer ID

--- scripts/teamviewer-get-id.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "regPath=HKEY_LOCAL_MACHINE\SOFTWARE\TeamViewer"
-set "regValue=ClientID"
-
-for /f "tokens=2*" %%a in ('reg query "%regPath%" /v "%regValue%" /t REG_DWORD ^| findstr /i "%regValue%"') do set "teamViewerID=%%b"
-echo %teamViewerID%
